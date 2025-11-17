79e7256646fb32d1b3184e455a0659181d14d646
fh96research <163102235+fh96research@users.noreply.github.com>
2025-11-17 14:29:22 +0100

    Add click handler to change title text and style

---
+++ script.js
@@ -0,0 +1,12 @@
+// Get elements
+const button = document.getElementById('btn');
+const title = document.getElementById('title');
+
+// Add click handler
+button.addEventListener('click', () => {
+    // Change text
+    title.textContent = 'JS is Running!';
+    
+    // Apply CSS class
+    title.classList.add('js-active');
+});
